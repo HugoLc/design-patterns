d228d8d4c0d56bd51a03cf88dbda4dec9a572a0a
Hugo <hugo@m3ecommerce.com>
2024-01-16 22:01:01 -0300

    feat: adapter pattern info

--- adapter_pattern/main.ts
+++ adapter_pattern/main.ts
@@ -1,12 +1,17 @@
+// The Adapter Pattern converts the interface of a class
+// into another interface the clients expect. Adapter lets
+// classes work together that couldn’t otherwise because of
+// incompatible interfaces.
+
 import TurkeyAdapter from "./adapters/TurkeyAdapter";
 import MallardDuck from "./duck/MallardDuck";
 import WildTurkey from "./turkey/WildTurkey";
 
-const mallard = new MallardDuck()
-const wildTurkey = new WildTurkey()
+const mallard = new MallardDuck();
+const wildTurkey = new WildTurkey();
 
-const turkeyAsDuck =  new TurkeyAdapter(wildTurkey)
+const turkeyAsDuck = new TurkeyAdapter(wildTurkey);
 
-wildTurkey.gobble()
-mallard.quack()
-turkeyAsDuck.quack()
+wildTurkey.gobble();
+mallard.quack();
+turkeyAsDuck.quack();
